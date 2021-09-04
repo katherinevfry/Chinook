4af93e5760f63f26780768240d2f631213291eb6
katherinevfry <katyvfry@gmail.com>
2021-09-04 10:46:47 -0500

    add

--- brazil-invoices.sql
+++ brazil-invoices.sql
@@ -1,1 +1,6 @@
 --brazil_customers_invoices
+SELECT FirstName, LastName, InvoiceId, InvoiceDate, BillingCountry
+FROM Customer c
+JOIN Invoice i
+ON c.CustomerId = i.CustomerId
+WHERE Country = 'Brazil'
