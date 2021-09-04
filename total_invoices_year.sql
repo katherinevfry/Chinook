adb474c910913da9d7f228f35e1d19afe60ee05d
katherinevfry <katyvfry@gmail.com>
2021-09-04 14:03:14 -0500

    update invoice year query

--- total_invoices_year.sql
+++ total_invoices_year.sql
@@ -1,7 +1,8 @@
 --8. `total_invoices_year.sql`: How many Invoices were there in 2009 and 2011?
 
-SELECT COUNT(*) numberOfInvoices
+SELECT
+    COUNT(CASE WHEN InvoiceDate BETWEEN '2009-01-01 00:00:00:000' AND '2009-12-31 00:00:00:000' THEN 1
+        ELSE NULL END) Invoices2009,
+    COUNT(CASE WHEN InvoiceDate BETWEEN '2011-01-01 00:00:00:000' AND '2011-12-31 00:00:00:000' THEN 1
+        ELSE NULL END)Invoices2011
 FROM Invoice
-WHERE InvoiceDate LIKE '%2011%'
-OR InvoiceDate LIKE '%2009%'
-
